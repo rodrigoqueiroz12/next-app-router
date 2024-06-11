1e0c89c61b33fbe52edda2e2855b57d474dcb475
Rodrigo Rocha de Queiroz <rodrigo@emperium.com.br>
2024-06-11 09:58:19 -0300

    update env

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,14 +1,17 @@
+import { createEnv } from '@t3-oss/env-nextjs'
 import { z } from 'zod'
 
-const envSchema = z.object({
-  NEXT_PUBLIC_API_BASE_URL: z.string().url(),
-  APP_URL: z.string().url(),
+export const env = createEnv({
+  server: {
+    APP_URL: z.string().url(),
+  },
+
+  client: {
+    NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+  },
+
+  runtimeEnv: {
+    APP_URL: process.env.APP_URL,
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  },
 })
-
-const _env = envSchema.safeParse(process.env)
-
-if (!_env.success) {
-  throw new Error('Missing environment variables')
-}
-
-export const env = _env.data
